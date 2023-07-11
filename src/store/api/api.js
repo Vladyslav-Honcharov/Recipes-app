24d1b188b7319ca6561195bc3caba9e3891b5e77
Vladyslav-Honcharov <tawzi14@gmail.com>
2023-07-11 11:05:52 +0300

    add firebase 2

--- src/store/api/api.js
+++ src/store/api/api.js
@@ -1,14 +1,24 @@
 // Импорт зависимостей
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  collection,
+  addDoc,
+  getDoc,
+  deleteDoc,
+  updateDoc,
+  doc,
+  getDocs,
+} from "firebase/firestore";
 import db from "../../firebase";
-
 // Создание экземпляра api
 export const api = createApi({
   reducerPath: "api",
   tagTypes: ["Recipe"],
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://recipe-app-841f4-default-rtdb.firebaseio.com/",
+    baseUrl: "https://recipe-app-841f4-default-rtdb.firebaseio.com", // Update the URL to the correct Firebase Realtime Database URL
+    auth: "DoX7uavrHuhZSnKNpBqiDId9UnOgDXUA7HWjl5mq",
   }),
+
   endpoints: (builder) => ({
     getRecipes: builder.query({
       query: (queryTerm) => `/`,
@@ -21,23 +31,23 @@ export const api = createApi({
     }),
     createRecipes: builder.mutation({
       async queryFn(recipe) {
-        const docRef = await db.collection("recipes").add(recipe);
-        const docSnapshot = await docRef.get();
+        const docRef = await addDoc(collection(db, "recipes"), recipe);
+        const docSnapshot = await getDoc(doc(db, "recipes", docRef.id));
         return docSnapshot.data();
       },
       invalidatesTags: [{ type: "Recipe" }],
     }),
     removeRecipe: builder.mutation({
       async queryFn(recipeId) {
-        await db.collection("recipes").doc(recipeId).delete();
+        await deleteDoc(doc(db, "recipes", recipeId));
         return recipeId;
       },
       invalidatesTags: [{ type: "Recipe" }],
     }),
     updateRecipe: builder.mutation({
       async queryFn({ id, ...recipe }) {
-        await db.collection("recipes").doc(id).update(recipe);
-        const docSnapshot = await db.collection("recipes").doc(id).get();
+        await updateDoc(doc(db, "recipes", id), recipe);
+        const docSnapshot = await getDoc(doc(db, "recipes", id));
         return docSnapshot.data();
       },
       invalidatesTags: [{ type: "Recipe" }],
